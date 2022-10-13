631e42de927ce5d87d8c956b4d22baa07f7bcbe2
Levi Jones <leviwill.j@gmail.com>
2022-10-12 20:55:41 -0600

    Did BOM assignment and linked from index

---
+++ lesson5/js/bom.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', () => {
+    let input = document.getElementById('favchap'),
+        listEle = document.getElementById('chaplist');
+
+    document.getElementById('favchapbtn').addEventListener('click', () => {
+        if (input.value) {
+            listEle.appendChild(createBOMRow(input.value));
+            input.value = '';
+            input.focus();
+        }
+    });
+
+    function createBOMRow(value) {
+        let li = document.createElement('li'),
+            button = document.createElement('button');
+
+        li.innerText = value;
+        button.innerHTML = '<i class="fa fa-xmark"></i>';
+        button.classList.add('list-x-button');
+        button.ariaLabel = 'Delete row button';
+        button.onclick = removeBOMRow;
+
+        li.appendChild(button);
+
+        return li;
+    }
+
+    function removeBOMRow() {
+        let li = this.parentElement;
+        li.parentElement.removeChild(li);
+        input.focus();
+    }
+});
