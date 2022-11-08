b0a792dfebda89f295f8dd6d1c99f9006173d056
Levi Jones <leviwill.j@gmail.com>
2022-11-08 14:50:52 -0700

    Updated windchill logic

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,6 +1,13 @@
-let temp = 55,
-    windspeed = 10,
-    chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
+let temp = 45,
+    windspeed = 8,
+    chill = null;
+    
+if (temp <= 50 && windspeed > 3) {
+    chill = Math.round(
+        (35.74 + (0.6215 * temp)) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temp * Math.pow(windspeed, 0.16))
+    );
+}
 
+document.getElementById('content__temp').innerText = temp + 'F';
 document.getElementById('wind-speed').innerText = windspeed + ' MPH';
-document.getElementById('wind-chill').innerText = chill;
+document.getElementById('wind-chill').innerText = (chill + 'F') ?? 'N/A';
