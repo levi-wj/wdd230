473dc9e75b899198c8e62da8419dd47a159041ce
Levi Jones <leviwill.j@gmail.com>
2022-11-11 23:11:29 -0700

    Added directory page

---
+++ chamber/js/directory.js
@@ -0,0 +1,91 @@
+var directory = (function () {
+    let curView = 'grid',
+        cardContainer = null,
+        listContainer = null,
+        cardButton = null,
+        listButton = null;
+    const companyData = (async () => {
+        // TODO: wdd230/
+        return await fetch(document.location.origin + '/chamber/data/data.json')
+            .catch(alert)
+            .then(response => response.json());
+        })();
+
+    window.addEventListener('load', async () => {
+        cardContainer = document.getElementById('main__card-section');
+        listContainer = document.getElementById('main__list-section');
+        cardButton = document.getElementById('discover__cardbutton');
+        listButton = document.getElementById('discover__listbutton');
+
+        companyData.then(companyJSON => {
+            companyJSON.forEach(company => {
+                let card = createCard(company),
+                    li = createLi(company);
+
+                cardContainer.appendChild(card);
+                listContainer.appendChild(li);
+            });
+        });
+    });
+
+    createCard = data => {
+        let card = document.createElement('section'),
+            img = document.createElement('img'),
+            name = document.createElement('h4'),
+            address = document.createElement('p'),
+            phone = document.createElement('p'),
+            site = document.createElement('a');
+
+        img.setAttribute('src', data.img);
+        name.textContent = data.name;
+        address.innerText = data.address;
+        phone.innerText = data.phone;
+        site.innerText = data.website;
+        site.href = data.website;
+
+        card.appendChild(img);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(site);
+
+        return card;
+    }
+
+    createLi = data => {
+        let row = document.createElement('tr'),
+            name = document.createElement('td'),
+            address = document.createElement('td'),
+            phone = document.createElement('td'),
+            sitetd = document.createElement('td'),
+            sitea = document.createElement('a');
+
+        name.textContent = data.name;
+        address.innerText = data.address;
+        phone.innerText = data.phone;
+        sitea.innerText = data.website;
+        sitea.href = data.website;
+        sitetd.appendChild(sitea);
+
+        row.appendChild(name);
+        row.appendChild(address);
+        row.appendChild(phone);
+        row.appendChild(sitetd);
+
+        return row;
+    }
+
+    changeView = view => {
+        if (curView !== view) {
+            curView = view;
+            cardContainer.classList.toggle('hidden');
+            listContainer.classList.toggle('hidden');
+            cardButton.classList.toggle('active');
+            listButton.classList.toggle('active');
+        }
+    }
+
+    return {
+        changeView: changeView
+    }
+})();
--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,23 +1,23 @@
 const requestURL = document.location.origin + '/wdd230/lesson9/data/prophets.json';
 
 fetch(requestURL)
-    .catch(err => alert)
+    .catch(alert)
     .then(response => response.json())
     .then(jsonObject => {
-        jsonObject.prophets.forEach(displayProphets);
+        jsonObject.prophets.forEach(displayProphet);
     });
 
-displayProphets = prophet => {
+displayProphet = pData => {
     let card = document.createElement('section'),
         img = document.createElement('img'),
         birthDate = document.createElement('p'),
         birthPlace = document.createElement('p'),
         h2 = document.createElement('h2');
 
-    h2.textContent = `${prophet.name} ${prophet.lastname}`;
-    birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
-    birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
-    img.setAttribute('src', prophet.imageurl);
+    h2.textContent = `${pData.name} ${pData.lastname}`;
+    birthDate.textContent = `Date of Birth: ${pData.birthdate}`;
+    birthPlace.textContent = `Place of Birth: ${pData.birthplace}`;
+    img.setAttribute('src', pData.imageurl);
 
     card.appendChild(h2);
     card.appendChild(birthDate);
