8a94a1559903a0d92ff7f1e16b86e8929a5cdd1f
Levi Jones <leviwill.j@gmail.com>
2022-11-15 16:24:54 -0700

    Added weather api page and improved mobile chamber page

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,26 @@
+(async () => {
+    const APIKEY = '90427170ad700f5d9f60d25078b99c82',
+        LAT = '64.8378',
+        LON = '-147.7164',
+        APIURL = `https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&q=Fairbanks&units=units=imperial&appid=${APIKEY}`,
+        response = await fetch(APIURL);
+
+    weather = await response.json();
+    console.log(weather)
+    showWeather(weather);
+})();
+
+showWeather = (weatherData) => {
+    let curTempEle = document.getElementById('current-temp'),
+        iconPathEle = document.getElementById('icon-src'),
+        iconEle = document.getElementById('weathericon')
+        figurecaption = document.querySelector('figcaption');
+    const ICONURL = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`,
+        temp = Math.round((weatherData.main.temp - 273.15)* 1.8000 + 32.00);
+
+    curTempEle.textContent = temp;
+    iconPathEle.textContent = ICONURL;
+    iconEle.setAttribute('src', ICONURL);
+    iconEle.setAttribute('alt', weatherData.weather[0].description);
+    figurecaption.textContent = weatherData.weather[0].description;
+}
