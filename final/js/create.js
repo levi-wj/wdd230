b107852d28b853fddcb9a7c58816a8745b0a8793
Levi Jones <leviwill.j@gmail.com>
2022-12-08 22:26:38 -0700

    Progress on spotify API

--- final/js/create.js
+++ final/js/create.js
@@ -1,9 +1,9 @@
 var create = (function () {
     const CLIENT_ID = '6dc1983dc35340d19a62a00846e9efee',
         CLIENT_SECRET = '299d0859973d465bb88ad12e00c367ae',
-        SCOPES = 'user-top-read',
-        REDIRECT = 'https://levi-wj.github.io/wdd230/final/create.html',
-        ENDPOINT = 'https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=3';
+        // SCOPES = 'user-top-read',
+        SCOPES = 'user-read-private%20user-read-email%20user-top-read',
+        MYURI = window.location.origin + window.location.pathname;
     let linkBtn = null,
         linkTxt = null,
         authCode = '',
@@ -25,14 +25,33 @@ var create = (function () {
     });
 
     const linkSpotify = () => {
-        window.location = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&scope=${SCOPES}&redirect_uri=${REDIRECT}`;
+        if (!authCode) {
+            window.location = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&scope=${SCOPES}&redirect_uri=${MYURI}`;
+        }
     }
     
     const getToken = async () => {
-        fetch(`https://accounts.spotify.com/api/token?code=${authCode}&redirect_uri=${REDIRECT}&grant_type=authorization_code`, { mode: 'no-cors'})
-            .then(res => {
-                console.log(res)
-            });
+        const res = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET),
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'grant_type=client_credentials'
+        });
+        const content = await res.json();
+        authToken = content.access_token;
+        console.log(content);
+
+        let topRes = await fetch(`https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=3`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${authToken}`,
+                'Content-Type': 'application/json'
+            }
+        });
+        // let json = await topRes.json();
+        // console.log(json)
     }
 
     return {};
